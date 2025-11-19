c469b35e478f2f27424dbc9a78c8dc538ee8f5ba
Leszek Lewandowski <Leszek.Lewandowski@dnv.com>
2025-11-19 14:46:04 +0100

    Page object pattern

---
+++ ComarchCwiczenia/Tests/ComarchCwiczenia.Tests.E2e/PageObjects/LoginPage.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+
+namespace ComarchCwiczenia.Tests.E2e.PageObjects;
+
+public class LoginPage(IWebDriver driver)
+{
+    public IWebElement UserField => driver.FindElement(By.Id("username"));
+    public IWebElement PasswordField => driver.FindElement(By.Id("password"));
+    public IWebElement LoginBtn => driver.FindElement(By.XPath("//*[@id=\"login\"]/button"));
+    public IWebElement ErrorMessage = driver.FindElement(By.CssSelector(".flash.error"));
+
+    public void EnterUserName(string userName)
+    {
+        UserField.SendKeys(userName);
+    }
+
+    public void EnterPassword(string password)
+    {
+        PasswordField.SendKeys(password);
+    }
+
+    public void ClickLoginButton()
+    {
+        LoginBtn.Click();
+    }
+
+    public bool IsErrorMessageDisplayed()
+    {
+        return ErrorMessage.Displayed;
+    }
+
+
+}
---
+++ ComarchCwiczenia/Tests/ComarchCwiczenia.Tests.E2e/Scenarios/TestBase.cs
@@ -0,0 +1,39 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComarchCwiczenia.Tests.E2e.Scenarios;
+
+public abstract class TestBase
+{
+    protected IWebDriver driver;
+
+    [SetUp]
+    public virtual void Setup()
+    {
+        new WebDriverManager.DriverManager().SetUpDriver(
+            new WebDriverManager.DriverConfigs.Impl.ChromeConfig());
+
+        var options = new ChromeOptions();
+        options.AddUserProfilePreference("download.default_directory", Path.Combine(Environment.CurrentDirectory, "Download"));
+        options.AddUserProfilePreference("download.prompt_for_download", false);
+
+        options.AddArgument("headless");
+        options.AddArgument("--disable-gpu");
+        options.AddArgument("--window-size=1920,1080");
+
+        driver = new ChromeDriver(options);
+        driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+    }
+
+    [TearDown]
+    public virtual void Teardown()
+    {
+        driver.Quit();
+        driver.Dispose();
+    }
+}
